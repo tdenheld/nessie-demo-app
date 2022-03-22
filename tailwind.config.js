24bc7d1227d4400bfd63f2ba90273c849165e586
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2022-03-22 14:32:45 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [require("@nessie/web-fundamentals/build/web/tailwind.config")],
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
