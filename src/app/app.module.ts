0099fbc1ac7a7313c94de83e5cb8180263b5286b
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2022-03-22 21:21:21 +0100

    add nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,19 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SiteNavComponent } from './site-nav/site-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteNavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+  ],
+  exports: [
+    SiteNavComponent,
   ],
   providers: [],
   bootstrap: [AppComponent],
