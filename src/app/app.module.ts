622c8e9c26cbd864126abfdbcb8d8af3f69cd4a5
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2022-03-22 22:30:33 +0100

    add form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,27 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
 import { SiteNavComponent } from './site-nav/site-nav.component';
+import { PageFormComponent } from './page-form/page-form.component';
+import { PageHomeComponent } from './page-home/page-home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: PageHomeComponent },
+  { path: 'form', component: PageFormComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     SiteNavComponent,
+    PageFormComponent,
+    PageHomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-  ],
-  exports: [
-    SiteNavComponent,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent],
