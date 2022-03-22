e941264ec77934b7905206c5eeb66ca630c202e8
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2022-03-22 17:27:22 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @NgModule({
   declarations: [
@@ -12,8 +10,6 @@ import { AngularSvgIconModule } from 'angular-svg-icon';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    AngularSvgIconModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
