97d1081ad392c25849dde15cab60a8b22049d584
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2022-03-23 23:03:56 +0100

    update form

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -6,10 +6,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class PageFormComponent implements OnInit {
-
+  mockGroupedAPI = [{
+    heading: "Favorieten",
+    items: [{
+      label: "Thuis",
+      subtext: "Utrecht Centraal",
+      icon: "station",
+      button: {
+        ariaLabel: "Menu",
+        icon: "menu-dots",
+      },
+    },],
+  },
+  {
+    heading: "Eerder gekozen",
+    items: [{
+      label: "Deventer",
+      subtext: "Treinstation",
+      icon: "station",
+      button: {
+        ariaLabel: "Favorite",
+        icon: "heart",
+      },
+    },
+    {
+      label: "Rotterdam Alexander",
+      subtext: "Treinstation",
+      icon: "station",
+      button: {
+        ariaLabel: "Favorite",
+        icon: "heart",
+      },
+    },
+    {
+      label: "Leeuwarden Camminghaburen",
+      subtext: "Treinstation",
+      icon: "station",
+      button: {
+        ariaLabel: "Favorite",
+        icon: "heart",
+      },
+    },
+    {
+      label: "Bunnik",
+      subtext: "Treinstation",
+      icon: "station",
+      button: {
+        ariaLabel: "Favorite",
+        icon: "heart",
+      },
+    },
+    ],
+  },
+  ];
+  transportOptions = [{
+    value: true,
+    label: "OV-chipkaart",
+  },
+  {
+    value: false,
+    label: "Eenmalige chipkaart",
+    helpText: "Papieren kaartje met chip erin",
+  },
+  {
+    value: false,
+    label: "E-ticket of mobiel ticket",
+  },
+  ];
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void { }
 }
