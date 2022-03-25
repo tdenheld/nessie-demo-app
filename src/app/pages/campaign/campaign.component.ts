44343d124752ca7188f4c94b0e1ef1f8c30af51e
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2022-03-25 15:34:50 +0100

    update

--- src/app/pages/campaign/campaign.component.ts
+++ src/app/pages/campaign/campaign.component.ts
@@ -6,7 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./campaign.component.css']
 })
 export class PageCampaignComponent implements OnInit {
-
+  usps = [{
+    title: 'Maandelijks wijzigen of opzeggen',
+    description: 'Je reist altijd met het voordeel dat past bij je reizen. En je zit nergens aan vast.',
+    icon: 'filter',
+  },
+  {
+    title: 'Betaal reiskosten maandelijks achteraf',
+    description: 'Je betaalt je reiskosten voor trein, bus, tram en metro maandelijks via automatische incasso.',
+    icon: 'invoice',
+  },
+  {
+    title: 'Reis direct met trein, bus, tram en metro',
+    description: 'Je checkt zorgeloos in en uit bij de trein, bus, tram en metro zonder dat je saldo nodig hebt.',
+    icon: 'tram',
+  },];
   constructor() { }
   ngOnInit(): void { }
 }
