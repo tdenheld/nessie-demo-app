b44429051698c59a3f55d9ab9a6dcce46690d666
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2022-03-28 22:43:34 +0200

    add faq

--- src/app/pages/campaign/campaign.component.ts
+++ src/app/pages/campaign/campaign.component.ts
@@ -39,6 +39,29 @@ export class PageCampaignComponent implements OnInit {
     icon: 'swap-horizontal',
   },];
 
+  faq = [
+    {
+      heading: "Wat is NS Flex voor abonnement?",
+      body: "Met NS Flex kun je elke maand de korting kiezen die past bij je reizen. En bespaar je flink op je reiskosten. Je hoeft niet meer minimaal € 20 of € 10 saldo te laden op je OV-chipkaart. Je betaalt namelijk maandelijks achteraf via automatische incasso."
+    },
+    {
+      heading: "Krijg ik met NS Flex ook korting in bus, tram en metro?",
+      body: "De korting van je NS Flex-abonnement geldt alleen op je treinreizen van NS en andere treinvervoerders. In de bus, metro en tram betaal je vol tarief en krijg je dus geen korting. Heb je een kortingsabonnement voor bus, metro of tram? Neem dan contact op met onze Klantenservice. We kunnen je kortingsabonnement dan met je NS Flex-abonnement combineren."
+    },
+    {
+      heading: "Hoe wijzig ik mijn abonnement?",
+      body: "Je kunt je abonnement elke maand wijzigen door in te loggen op Mijn NS. Hier zie je ook direct welk abonnement je nu hebt. Heb je een Reizen op saldo-abonnement? Dan kun je ook via Mijn NS overstappen op een NS Flex-abonnement. Het saldo dat nog op je OV-chipkaart staat verrekenen we automatisch op je eerste NS Flex-factuur. Lees meer op de pagina Abonnement wijzigen."
+    },
+    {
+      heading: "Ik reis met Keuzedagen. Kan dat ook met een NS Flex-abonnement?",
+      body: "Je kunt nog niet met Keuzedagen reizen bij NS Flex. Als je je Keuzedagen wilt blijven gebruiken, kun je dus beter nog niet overstappen naar een NS Flex-abonnement."
+    },
+    {
+      heading: "Kan ik een NS Flex-abonnement combineren met een Studentenreisproduct?",
+      body: "Je kunt naast je Studentenreisproduct ook met een Flex-abonnement reizen, maar let op: dat kan niet op dezelfde OV-chipkaart. Bestel dus je NS Flex-abonnement op een nieuwe OV-chipkaart. Dit geldt ook voor andere reisproducten in combinatie met een Studentenreisproduct. Zo voorkom je een onverwachte rekening aan het einde van de maand."
+    }
+  ];
+
   subscriptions = subscriptions.filter(subs =>
     subs.title === 'Weekend Voordeel' ||
     subs.title === 'Dal Voordeel' ||
