0a13b4ae06b580a52ecf5047a865ebfdd096e820
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2022-04-11 21:46:00 +0200

    update fromTo

--- src/app/pages/kaartjes/kaartjes.component.ts
+++ src/app/pages/kaartjes/kaartjes.component.ts
@@ -6,58 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kaartjes.component.css']
 })
 export class PageKaartjesComponent implements OnInit {
-  mockGroupedAPI = [{
-    heading: "Favorieten",
-    items: [{
-      label: "Thuis",
-      subtext: "Utrecht Centraal",
-      icon: "station",
-      button: {
-        ariaLabel: "Menu",
-        icon: "menu-dots",
-      },
-    },],
-  },
-  {
-    heading: "Eerder gekozen",
-    items: [{
-      label: "Deventer",
-      subtext: "Treinstation",
-      icon: "station",
-      button: {
-        ariaLabel: "Favorite",
-        icon: "heart",
-      },
+  fromActive = false;
+  toActive = false;
+
+  mockGroupedAPI = [
+    {
+      heading: "Favorieten",
+      items: [{
+        label: "Utrecht Centraal",
+        subtext: "Thuis",
+        icon: "station",
+      },],
     },
     {
-      label: "Rotterdam Alexander",
-      subtext: "Treinstation",
-      icon: "station",
-      button: {
-        ariaLabel: "Favorite",
-        icon: "heart",
+      heading: "Eerder gekozen",
+      items: [{
+        label: "Deventer",
+        subtext: "Treinstation",
+        icon: "station",
       },
-    },
-    {
-      label: "Leeuwarden Camminghaburen",
-      subtext: "Treinstation",
-      icon: "station",
-      button: {
-        ariaLabel: "Favorite",
-        icon: "heart",
+      {
+        label: "Rotterdam Alexander",
+        subtext: "Treinstation",
+        icon: "station",
       },
-    },
-    {
-      label: "Bunnik",
-      subtext: "Treinstation",
-      icon: "station",
-      button: {
-        ariaLabel: "Favorite",
-        icon: "heart",
+      {
+        label: "Leeuwarden Camminghaburen",
+        subtext: "Treinstation",
+        icon: "station",
       },
+      {
+        label: "Bunnik",
+        subtext: "Treinstation",
+        icon: "station",
+      },
+      ],
     },
-    ],
-  },
   ];
 
   constructor() { }
