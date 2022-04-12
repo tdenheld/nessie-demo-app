e708ec1eb23c95f54b0ea6aff42a157858e0a8dd
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2022-04-12 16:45:59 +0200

    update

--- src/app/pages/kaartjes/kaartjes.component.ts
+++ src/app/pages/kaartjes/kaartjes.component.ts
@@ -13,17 +13,34 @@ export class PageKaartjesComponent implements OnInit {
     {
       title: 'Dagretour',
       icon: 'round-trip',
-      body: 'Heen en weer op hetzelfde traject, op je mobiel of printen.'
+      body: 'Heen en weer op hetzelfde traject, op je mobiel of printen.',
     },
     {
       title: 'Enkele reis',
       icon: 'one-way',
-      body: 'Een enkele reis naar je bestemming, op je mobiel of printen.'
+      body: 'Een enkele reis naar je bestemming, op je mobiel of printen.',
     },
     {
       title: 'Railrunner',
       icon: 'children',
-      body: 'Voordelig reizen voor kinderen, op je mobiel of printen.'
+      body: 'Voordelig reizen voor kinderen, op je mobiel of printen.',
+      price: 2.5,
+      priceTrailing: '/ kind'
+    },
+    {
+      title: 'Groepsticket Daluren',
+      icon: 'group',
+      body: 'Voordelig reizen met een groep vanaf 2 personen. Geldig tijdens daluren.',
+      price: 6.3,
+      priceLeading: 'Vanaf',
+      priceTrailing: '/ persoon'
+    },
+    {
+      title: 'Samenreisticket Dagretour Dal',
+      icon: 'co-traveler',
+      body: 'Reis voordelig mee met iemand die een NS-abonnement of Studentenreisproduct heeft.',
+      discount: '40%',
+      discountTrailing: 'Korting op een dagretour'
     }
   ];
 
