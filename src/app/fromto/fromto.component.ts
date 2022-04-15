44dbc524cc3c669dde3c6205320e4bb5cad5f49f
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2022-04-15 12:56:11 +0200

    add station api

--- src/app/fromto/fromto.component.ts
+++ src/app/fromto/fromto.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-fromto',
@@ -7,44 +7,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FromtoComponent implements OnInit {
   fromActive = false;
-  toActive = false;
+  fromFocus = false;
+  toFocus = false;
+  swap = false;
+
+  mobile = true;
+
+  getMobile() {
+    requestAnimationFrame(() => {
+      this.mobile = innerWidth < 740 || false;
+    });
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.getMobile();
+  }
+
+  async getStations(value: string) {
+    const response = await fetch(`https://gateway.apiportal.ns.nl/rio-autosuggest-api/stations?q=${value}&countryCode=nl`, {
+      headers: {
+        'Ocp-Apim-Subscription-Key': '5f6d82eb0e6b412f99d35c4125982b80'
+      }
+    });
+    return await response.json();
+  }
 
   mockGroupedAPI = [
     {
-      heading: "Favorieten",
+      heading: 'Favorieten',
       items: [{
-        label: "Utrecht Centraal",
-        subtext: "Thuis",
-        icon: "station",
+        label: 'Utrecht Centraal',
+        subtext: 'Thuis',
+        icon: 'station',
       },],
     },
     {
-      heading: "Eerder gekozen",
+      heading: 'Eerder gekozen',
       items: [{
-        label: "Deventer",
-        subtext: "Treinstation",
-        icon: "station",
+        label: 'Deventer',
+        subtext: 'Treinstation',
+        icon: 'station',
       },
       {
-        label: "Rotterdam Alexander",
-        subtext: "Treinstation",
-        icon: "station",
+        label: 'Rotterdam Alexander',
+        subtext: 'Treinstation',
+        icon: 'station',
       },
       {
-        label: "Leeuwarden Camminghaburen",
-        subtext: "Treinstation",
-        icon: "station",
+        label: 'Leeuwarden Camminghaburen',
+        subtext: 'Treinstation',
+        icon: 'station',
       },
       {
-        label: "Bunnik",
-        subtext: "Treinstation",
-        icon: "station",
+        label: 'Bunnik',
+        subtext: 'Treinstation',
+        icon: 'station',
       },
       ],
     },
   ];
 
   constructor() { }
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.getMobile();
 
+    this.getStations('ut').then((arr) => {
+      arr.map((val: any) => {
+        console.log(val.naam);
+      });
+    });
+  }
 }
