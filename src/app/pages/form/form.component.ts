825b4dff1c98131c0ee551b9c9517bae43f71841
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2022-04-30 15:33:16 +0200

    update

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -8,53 +8,53 @@ import { Component, OnInit } from '@angular/core';
 export class PageFormComponent implements OnInit {
 
   mockGroupedAPI = [{
-    heading: "Favorieten",
+    heading: 'Favorieten',
     items: [{
-      label: "Thuis",
-      subtext: "Utrecht Centraal",
-      icon: "station",
+      label: 'Thuis',
+      subtext: 'Utrecht Centraal',
+      icon: 'station',
       button: {
-        ariaLabel: "Menu",
-        icon: "menu-dots",
+        ariaLabel: 'Menu',
+        icon: 'menu-dots',
       },
     },],
   },
   {
-    heading: "Eerder gekozen",
+    heading: 'Eerder gekozen',
     items: [{
-      label: "Deventer",
-      subtext: "Treinstation",
-      icon: "station",
+      label: 'Deventer',
+      subtext: 'Treinstation',
+      icon: 'station',
       button: {
-        ariaLabel: "Favorite",
-        icon: "heart",
+        ariaLabel: 'Favorite',
+        icon: 'heart',
       },
     },
     {
-      label: "Rotterdam Alexander",
-      subtext: "Treinstation",
-      icon: "station",
+      label: 'Rotterdam Alexander',
+      subtext: 'Treinstation',
+      icon: 'station',
       button: {
-        ariaLabel: "Favorite",
-        icon: "heart",
+        ariaLabel: 'Favorite',
+        icon: 'heart',
       },
     },
     {
-      label: "Leeuwarden Camminghaburen",
-      subtext: "Treinstation",
-      icon: "station",
+      label: 'Leeuwarden Camminghaburen',
+      subtext: 'Treinstation',
+      icon: 'station',
       button: {
-        ariaLabel: "Favorite",
-        icon: "heart",
+        ariaLabel: 'Favorite',
+        icon: 'heart',
       },
     },
     {
-      label: "Bunnik",
-      subtext: "Treinstation",
-      icon: "station",
+      label: 'Bunnik',
+      subtext: 'Treinstation',
+      icon: 'station',
       button: {
-        ariaLabel: "Favorite",
-        icon: "heart",
+        ariaLabel: 'Favorite',
+        icon: 'heart',
       },
     },
     ],
@@ -63,47 +63,47 @@ export class PageFormComponent implements OnInit {
 
   transportOptions = [{
     value: true,
-    label: "OV-chipkaart",
+    label: 'OV-chipkaart',
   },
   {
     value: false,
-    label: "Eenmalige chipkaart",
-    helpText: "Papieren kaartje met chip erin",
+    label: 'Eenmalige chipkaart',
+    helpText: 'Papieren kaartje met chip erin',
   },
   {
     value: false,
-    label: "E-ticket of mobiel ticket",
+    label: 'E-ticket of mobiel ticket',
   },
   ];
 
   klasse = [{
     value: true,
-    label: "2e klas",
+    label: '2e klas',
     checked: true,
   },
   {
     value: false,
-    label: "1e klas",
+    label: '1e klas',
   },
   ];
 
   typeTravel = [{
     value: true,
-    label: "Enkele reis",
+    label: 'Enkele reis',
     checked: true,
   },
   {
     value: false,
-    label: "Retour",
+    label: 'Retour',
   },
   ];
 
   constructor() { }
   ngOnInit(): void {
-    document.querySelector("html")?.classList.add("nes-bg-elevated");
+    document.querySelector('html')?.classList.add('nes-bg-elevated');
   }
 
   ngOnDestroy() {
-    document.querySelector("html")?.classList.remove("nes-bg-elevated");
+    document.querySelector('html')?.classList.remove('nes-bg-elevated');
   }
 }
