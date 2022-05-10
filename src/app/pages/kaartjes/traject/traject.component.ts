0784a74c532f3155ead434b085b3a84fc5dca984
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2022-05-10 11:31:57 +0200

    update traject

--- src/app/pages/kaartjes/traject/traject.component.ts
+++ src/app/pages/kaartjes/traject/traject.component.ts
@@ -6,11 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./traject.component.css']
 })
 export class PageTrajectComponent implements OnInit {
-  usps = [
+  public usps = [
     "Geen toeslag van € 1 op je treinkaartje",
     "Geen wachtrijen bij de balie of kaartautomaat",
     "Direct op pad!",
   ];
+
+  public specs = [
+    {
+      icon: "clock",
+      label: "28 min reistijd"
+    },
+    {
+      icon: "map",
+      label: "39 km afstand"
+    },
+    {
+      icon: "train",
+      label: "172 dagelijkse treinen"
+    }
+  ]
 
   constructor() { }
   ngOnInit(): void { }
