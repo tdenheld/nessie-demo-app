0bb31192135d1e08fba3edfbb1f463bb3d5e5351
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2022-07-06 21:30:38 +0200

    update

--- src/app/components/add-trip/add-trip.component.ts
+++ src/app/components/add-trip/add-trip.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-add-trip',
@@ -9,4 +9,5 @@ export class AddTripComponent implements OnInit {
   constructor() { }
   ngOnInit(): void { }
 
+  @Input() active:boolean = false;
 }
--- src/app/components/receipt-keuzehulp/receipt-keuzehulp.component.ts
+++ src/app/components/receipt-keuzehulp/receipt-keuzehulp.component.ts
@@ -6,10 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./receipt-keuzehulp.component.scss']
 })
 export class ReceiptKeuzehulpComponent implements OnInit {
-
   constructor() { }
+  ngOnInit(): void { }
 
-  ngOnInit(): void {
-  }
-
+  public addTripActive: boolean = false;
 }
