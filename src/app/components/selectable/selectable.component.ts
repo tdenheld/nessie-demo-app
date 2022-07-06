8e4e36d0620eda5ec735403e7e3fa8f203e5b083
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2022-07-06 21:09:02 +0200

    update border-alpha dark mode

--- src/app/components/selectable/selectable.component.ts
+++ src/app/components/selectable/selectable.component.ts
@@ -8,7 +8,6 @@ import { Component, Input, OnInit } from '@angular/core';
 
 export class SelectableComponent implements OnInit {
   @Input() active: boolean = false;
-  @Input() selectable: boolean = true;
 
   constructor() { }
   ngOnInit(): void { }
