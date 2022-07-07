c338f0eb434b03de5310141fd3e8edcb2cc12d82
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2022-07-07 11:17:04 +0200

    update selectable

--- src/app/components/selectable/selectable.component.ts
+++ src/app/components/selectable/selectable.component.ts
@@ -8,8 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 
 export class SelectableComponent implements OnInit {
   @Input() active: boolean = false;
-
+  @Input() selectable: boolean = true;
+  
   constructor() { }
   ngOnInit(): void { }
-
 }
